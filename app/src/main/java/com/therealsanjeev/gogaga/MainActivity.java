921b656b2111e3fa60da9873f1f7aa41203744bd
Prateek <codes.prateek@gmail.com>
2020-10-14 19:58:03 +0530

    Added some code
    This code will definetly do something.

--- app/src/main/java/com/therealsanjeev/gogaga/MainActivity.java
+++ app/src/main/java/com/therealsanjeev/gogaga/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //This couple of code doesn't make any sense but still doing
+        TextView textView = (TextView) findViewById(R.id.text_1);
+        textView.setText("Ready to Go");
     }
 }
